b6c97a88f5504923edc3fbb6a39cbeda402cefcb
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-07 18:23:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,8 +14,8 @@ def introduction_with_language(name, language)
 end
 
 
-def introduction_with_language_optional(name, Ruby)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{Ruby}."
 end 
 
 
